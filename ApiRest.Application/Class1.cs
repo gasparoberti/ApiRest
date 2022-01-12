0943da7540f44f63a2225d87256f32712284addf
Gaspar <obertigaspar@gmail.com>
2022-01-12 12:24:18 -0300

    Se crearon los proyectos de cada capa (bibliotecas de clases) y sus respectivas referencias.

---
+++ ApiRest.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiRest.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ ApiRest.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ApiRest.Repository
+{
+    public class Class1
+    {
+    }
+}
