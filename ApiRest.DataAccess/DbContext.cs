fddbeae77f9666739951d72a0e08feecb7709ea4
Gaspar <obertigaspar@gmail.com>
2022-01-13 15:12:24 -0300

    Se crearon los elementos que se describen en los comentarios 1 y 2 del [Issue #3]

---
+++ ApiRest.DataAccess/DbContext.cs
@@ -0,0 +1,36 @@
+﻿using ApiRest.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace ApiRest.DataAccess
+{
+    public class DbContext<T> : IDbContext<T>
+    {
+        IList<T> _data;
+
+        public DbContext()
+        {
+            _data = new List<T>();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Save(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ApiRest.Entities/FootballTeam.cs
+++ ApiRest.Entities/FootballTeam.cs
@@ -2,7 +2,7 @@
 
 namespace ApiRest.Entities
 {
-    public class FootballTeam
+    public class FootballTeam : Entity
     {
         public string Nombre { get; set; }
         public double Score { get; set; }
