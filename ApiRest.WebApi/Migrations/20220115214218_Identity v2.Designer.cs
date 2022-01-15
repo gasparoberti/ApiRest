b292a41966d4f0ea4771784e542ccfca2a7181e2
Gaspar <obertigaspar@gmail.com>
2022-01-15 18:45:05 -0300

    se agregó Identity

--- ApiRest.WebApi/Migrations/20220115214218_Identity v2.Designer.cs
+++ ApiRest.WebApi/Migrations/20220115214218_Identity v2.Designer.cs
@@ -4,14 +4,16 @@ using ApiRest.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ApiRest.WebApi.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220115214218_Identity v2")]
+    partial class Identityv2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
